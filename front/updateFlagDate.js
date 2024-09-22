ac14567434655eb4e4a6e9584399196608984e51
DevinciAlex <alexandre.manceau@edu.devinci.fr>
2024-09-22 23:02:24 +0200

    add a space after date :

--- front/updateFlagDate.js
+++ front/updateFlagDate.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function updateDate() {
         const now = new Date();
         const dateString = now.toLocaleDateString();
-        document.getElementById('current-date').textContent = `Date :${dateString}`;
+        document.getElementById('current-date').textContent = `Date : ${dateString}`;
     }
 
     window.onload = updateDate;
